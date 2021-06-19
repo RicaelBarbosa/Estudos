f86e3ec9025248f3b08513d53cc27688d3a179c3
Ricael Pereira Barbosa <85632605+RicaelBarbosa@users.noreply.github.com>
2021-06-19 15:45:34 -0300

    Add files via upload

---
+++ JavaScript/ex001/script.js
@@ -0,0 +1,3 @@
+alert('Minha primeira mensagem') //Alerta de mensagem ou aviso.
+confirm('Está gostando de JS?') //Janela com o Botão Ok e Canelar
+prompt('Qual seu nome?') //Pergunta com caixa de resposta
---
+++ JavaScript/ex002/script.js
@@ -0,0 +1,2 @@
+var nome = prompt('Qual seu nome?') //Pergunta com caixa de resposta
+alert('É um grande prazer te conhecer ' + nome + '!')
